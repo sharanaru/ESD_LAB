a103be6ac3777ed7ad3455bf84218fa5dc7ca826
sharanaru <55450674+sharanaru@users.noreply.github.com>
2019-10-16 00:58:34 -0600

    Update main.c
    Working buffer allocations, delete, writing to buffer0

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 #include<ctype.h>
 #include <at89c51ed2.h>
 #define HEAP_SIZE 4000
+#define ask_allocation "Enter number of bytes for initial allocation\n\r"
+#define ask_allocation_new "enter number of bytes for allocation between 30 and 300 \n\r"
 #define message_pass "Allocation was succesful\n\r"
 #define message_initial_fail "Enter a different value\n\r"
 #define message_fail "Allocation failed\n\r"
+#define max_no_buffers (4000-(2*32))/30
 __xdata char __sdcc_heap[HEAP_SIZE];
 const unsigned int __sdcc_heap_size = HEAP_SIZE;
 
 
-extern int putchar(int c)
+ int putchar (int c)
 {
 	while (!TI);				// compare asm code generated for these three lines
 	while (TI == 0);
@@ -23,7 +26,7 @@ extern int putchar(int c)
 }
 
 
-extern int getchar(void)
+ int getchar(void)
 {
 
     while (!RI);                // compare asm code generated for these three lines
@@ -42,6 +45,7 @@ int putstr (char *s)
 	//putchar('\n');
 	return i+1;
 }
+
 
 
 
@@ -70,44 +74,135 @@ bool checkmalloc(int *test)
         return true;
 
 }
+struct bufferinfo
+{
+uint16_t buffersize;
+uint16_t bufferaddress;
+uint16_t bufferend;
+};
+int readint()
+{
+    char *reader;
+    gets(reader);int number=atoi(reader);
+    return number;
+
+}
+
+
 
 void main()
 {
-    bool status=true;
-    uint16_t input;
+bool status=true;
+int counter=0;
+uint16_t initial_allocation;
+uint16_t overall_character_count=0;char *buffers[max_no_buffers];//array of pointers with no. of elements being maximum no. of buffers that can be allocated
+//to keep track of buffers allocated
 
-    while(status)
-    {
-    putstr("Enter number of bytes for initial allocation\n\r");
-    input= getchar();
-    if((input>=32) && (input<=3200) &&(input%16==0))
-    {
-     char *buffer_0=malloc(input);
-     char *buffer_1=malloc(input);
-     if(checkmalloc(buffer_1)==false)
+//char *reader;
+
+while(status==true)
+{
+putstr(ask_allocation);
+//gets(reader);
+initial_allocation=readint();
+if((initial_allocation>=32) && (initial_allocation<=3200) &&(initial_allocation%16==0))
+{
+     buffers[counter]=malloc(initial_allocation);
+    counter++;
+     buffers[counter]=malloc(initial_allocation);
+    counter++;
+    if((checkmalloc(buffers[0])==false)||(checkmalloc(buffers[1])==false))
         {
-            free(buffer_0);free(buffer_1);
-            putstr(message_initial_fail);
+        free(buffers[1]);
+        counter--;
+        free(buffers[0]);counter--;
+        putstr(message_initial_fail);
         }
-      else
-      {
+    else
+        {
         putstr(message_pass);
+
         status=false;
-      }
+        }
+    }
+else
+    putstr(message_initial_fail);
+}
+
+struct bufferinfo buffers_[max_no_buffers];
+char *activebuffers=malloc(sizeof(char)*max_no_buffers);activebuffers[0]='1';activebuffers[1]='1';
+
+int receiveinput;
+while(1)
+{
+   receiveinput=getchar();
+   switch(receiveinput)
+   {
+   case '+':
+    putstr(ask_allocation_new);
+    counter++;
+    buffers_[counter].buffersize=readint();
+buffers[counter] =malloc(buffers_[counter].buffersize);
+    if(checkmalloc(buffers[counter])==false)
+    {
+     free(buffers[counter]);counter--;
+    putstr(message_initial_fail);
+
+    break;
     }
     else
-    putstr(message_initial_fail);
+    {
+    putstr(message_pass);activebuffers[counter]='1';
+    break;
     }
-struct bufferinfo
-{
-int buffersize;
-uint16_t 
-};
-int maximumbuffers=(HEAP_SIZE-(2*input))/30;//30 is minimum no. of bytes to be allocated; stores maximum number of buffers that can be allocated
-char* buffers[maximumbuffers];
-int counter=0;//to keep track of buffers allocated
-while
+   break;
+   case '-':
+    putstr("Enter buffer to be deleted\n\r");
+    int buffer_delete=getchar()-'0';
+    if(buffer_delete==0)
+putstr("Invalid input-Buffer 0 not to be deleted\n\r");
 
+else{
+    if(buffer_delete==1)
+    {
+        free(buffers[1]);
+        putstr("Buffer 1 deleted");
+    }
+    else
+{
+    if(buffer_delete<=counter)
+    {
+    free(buffers[buffer_delete]);
+    printf_tiny("Buffer %d was deleted",buffer_delete);
+    }
+    else
+        printf_tiny("Buffer %d not created yet",buffer_delete-'0');
 }
+break;
+   default:
+    if(islower(receiveinput))
+    {
+        buffers[0][overall_character_count]=receiveinput;printf_tiny("buffer 0 character %c\r\n",buffers[0][overall_character_count]);
+        overall_character_count++;
+    }
+    else
+        putchar(receiveinput);
+}
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
